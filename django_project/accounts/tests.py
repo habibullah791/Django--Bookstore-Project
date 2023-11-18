6e8790faf14af773fe732bc106a9319b311b20ff
habibullah791 <habibullahshahid001@gmail.com>
2023-11-17 16:11:50 -0800

    ADD accounts app and create custom model

---
+++ django_project/accounts/tests.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="will",
+            email="will123@gmail.com",
+            password="testpass123",
+        )
+
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will123@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username="will",
+            email="will123@gmail.com",
+            password="testpass123",
+        )
+        
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will123@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        

676dc1fe6ff699cd5344589a2c058cddc84bbc11
habibullah791 <habibullahshahid001@gmail.com>
2023-11-25 08:48:42 -0800

    Add - Reviews to the books

--- django_project/books/admin.py
+++ django_project/books/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewLine(admin.TabularInline):
+    model = Review
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "price",)
+    inlines = [
+        ReviewLine,
+    ]
+    list_display = (
+        "title",
+        "author",
+        "price",
+    )
 
 
 admin.site.register(Book, BookAdmin)

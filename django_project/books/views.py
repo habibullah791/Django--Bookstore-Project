e186155e0255e00b4e66010107d835d28a8c4a0a
habibullah791 <habibullahshahid001@gmail.com>
2023-11-26 09:49:07 -0800

    Add - Permissions

--- django_project/books/views.py
+++ django_project/books/views.py
@@ -1,13 +1,15 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from .models import Book
 
-class BookListView(ListView):
+
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     template_name = "books/book_detail.html"
